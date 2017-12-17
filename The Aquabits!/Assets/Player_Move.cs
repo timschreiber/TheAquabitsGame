4cefcd4c77fc87aa1e95a1852e132976a9fb4cd6
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-12-17 14:04:02 -0500

    Sprites, & Player R, L, & Jump

---
+++ The Aquabits!/Assets/Player_Move.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 1250;
+    private float moveX;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerMove();
+	}
+
+    void PlayerMove()
+    {
+        // Controls
+        moveX = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        // Animations
+        // Player Direction
+        if(moveX < 0.0f && !facingRight)
+        {
+            FlipPlayer();
+        }
+        else if(moveX > 0.0f && facingRight)
+        {
+            FlipPlayer();
+        }
+
+        // Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
