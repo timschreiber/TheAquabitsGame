97c2e1d7584b0469c7ea83e644720f34aecbc555
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-12-18 02:04:36 -0500

    Added player jump & death, and camera tracking

---
+++ The Aquabits!/Assets/Player_Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour
+{
+    public float deathY;
+    public bool hasDied;
+
+	// Use this for initialization
+	void Start ()
+    {
+        hasDied = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!hasDied && gameObject.transform.position.y < deathY)
+        {
+            hasDied = true;
+        }
+
+        if(hasDied)
+        {
+            die();
+        }
+	}
+
+    void die()
+    {
+        SceneManager.LoadScene("ManAnt_Scene");
+        hasDied = false;
+    }
+}
